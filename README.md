a4ffa3e628dbe83c685d5743a0034fc6f33c994a
liyan <liyan@wumii.com>
2019-12-24 09:59:20 +0800

    feat: 🎸 change readme

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # generatetypescriptinterface README
 
-This is the README for your extension "generatetypescriptinterface". After writing up a brief description, we recommend including the following sections.
+covert java class to typescript interface.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+1.support normal array
+2.and so on
 
 ## Requirements
 
@@ -24,8 +19,8 @@ For example:
 
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+- `myExtension.enable`: enable/disable this extension
+- `myExtension.thing`: set to `blah` to do something
 
 ## Known Issues
 
@@ -35,31 +30,23 @@ Calling out known issues can help limit users opening duplicate issues against y
 
 Users appreciate release notes as you update your extension.
 
-### 1.0.0
+### 0.0.1
 
 Initial release of ...
 
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
+---
 
 ## Working with Markdown
 
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+**Note:** You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
 
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+- Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+- Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
 
 ### For more information
 
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { Range } from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "generatetypescriptinterface" is now active!'
-  );
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "extension.tsGenerate",
     () => {
-      const select = vscode.window.activeTextEditor!.document.getText();
-      console.log("选中文本啦", select);
-      if (isValidSelect(select)) {
-        generate(select);
+      const editor = vscode.window.activeTextEditor!;
+      const selection = editor.selection;
+
+      if (!selection.isEmpty) {
+        editor.edit(builder => {
+          const start = selection.start.line;
+          const end = selection.end.line;
+          for (let i = start; i <= end; i += 1) {
+            const line = editor.document.lineAt(i);
+            const range = new Range(
+              i,
+              line.range.start.character,
+              i,
+              line.range.end.character
+            );
+            builder.replace(range, generate(line.text));
+          }
+        });
       }
     }
   );
@@ -28,14 +35,104 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-function isValidSelect(select: string) {
-  return select.length > 0;
+function generate(text: string): string {
+  function isJavaClass() {
+    return text.indexOf("class") !== -1;
+  }
+
+  function isJavaEnum() {
+    return text.indexOf("enum") !== -1;
+  }
+
+  function isPrimitives() {
+    const JavaType = [
+      "byte",
+      "int",
+      "integer ",
+      "long",
+      "float",
+      "double",
+      "string",
+      "boolean"
+    ];
+    return (
+      JavaType.find(v => text.toLocaleLowerCase().indexOf(v) !== -1) != null
+    );
+  }
+
+  function isStringOrBoolean() {
+    return (
+      ["String", "Boolean"].find(
+        v => text.toLocaleLowerCase().indexOf(v) !== -1
+      ) != null
+    );
+  }
+
+  function isList() {
+    return text.indexOf("<") !== -1;
+  }
+
+  const cleanText = text
+    .replace(";", "")
+    .replace(/\/\/.*/, "")
+    .replace(/\(.*/, "")
+    .trim();
+
+  function generateField() {
+    if (cleanText === "") {
+      return "";
+    } else if (cleanText === "}") {
+      return text;
+    } else if (!/\s/.test(cleanText)) {
+      return generateEnumField();
+    } else {
+      const texts = cleanText.split(" ");
+      const field = texts?.[2] ?? "";
+
+      if (isPrimitives()) {
+        const type = isStringOrBoolean()
+          ? (texts?.[1] ?? "").toLocaleLowerCase()
+          : "number";
+        return field + ":" + type;
+      } else if (isList()) {
+        const type = text.match(/<(.*?)>/)?.[1] ?? "";
+        return field + ":" + type + "[]";
+      } else {
+        return field + ":" + (texts?.[1] ?? "");
+      }
+    }
+  }
+
+  function generateEnumField() {
+    const texts = cleanText.toLocaleLowerCase().split("_");
+
+    return (
+      texts
+        .map((t, i) => {
+          return i !== 0
+            ? t.replace(/^\S/, s => {
+                return s.toUpperCase();
+              })
+            : t;
+        })
+        .join("") +
+      "=" +
+      "'" +
+      cleanText +
+      "',"
+    );
+  }
+
+  if (isJavaClass()) {
+    return cleanText
+      .replace(/(public|private)\s+class/i, "export interface")
+      .replace(/(implements|extends).*/, "{");
+  } else if (isJavaEnum()) {
+    return cleanText.replace(/(public|private)\s+enum/i, "export enum");
+  } else {
+    return generateField();
+  }
 }
 
-function generate(select: string): string {
-  return select;
-}
-
-function replaceSelect() {}
 // this method is called when your extension is deactivated
 export function deactivate() {}
